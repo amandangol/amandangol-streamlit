86810d56c22ff1b8aafb384cc4c62f64e3262845
Aman <icrextha@gmail.com>
2023-05-16 10:19:35 +0545

    fixing app.py

--- app.py
+++ app.py
@@ -50,7 +50,6 @@ options_acc_area = ['Other', 'Office areas', 'Residential areas', ' Church areas
 # features list
 features = ['Number_of_vehicles_involved', 'Number_of_casualties', 'Hour_of_Day', 'Type_of_collision', 'Age_band_of_driver', 'Sex_of_driver',
             'Educational_level', 'Service_year_of_vehicle', 'Day_of_week', 'Area_accident_occured']
-
 # Give a title to web app using html syntax
 st.markdown("<h1 style='text-align: center;'>Accident Severity Prediction App 🚧</h1>",
             unsafe_allow_html=True)
@@ -108,9 +107,8 @@ def main():
        [Kaggle](https://www.kaggle.com/avikumart) 
        """)
 
-        a, b, c = st.columns([0.2, 0.6, 0.2])
 
-
+a, b, c = st.columns([0.2, 0.6, 0.2])
 with b:
     st.image("banner-picture.jpeg", use_column_width=True)
 
